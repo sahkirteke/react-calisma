0f69faf34aa1fe85357e93b8a976bb2399d91eb6
sahkirteke <sahhusamettin@windowslive.com>
2020-12-22 14:21:14 +0300

    eject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 // import styled from "styled-components";
 
@@ -81,6 +81,10 @@ class App extends Component {
     //   cursor: "pointer",
     // };
     let persons= null;
+    let btnClass ='';
+
+
+
     if (this.state.showPersons ){
       persons= (
         <div>
@@ -113,24 +117,26 @@ class App extends Component {
         </div> 
       )
       // style.backgroundColor='red';
+
+      btnClass=classes.Red;
     }
 
-    let classes =[];
+    const assignedClasses =[];
     if(this.state.persons.length <=2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
       }
     if (this.state.persons.length<=1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     
     return (
-      <div className="App">
+      <div className={classes.App}>
         
         <h1>React projem</h1>
-        <p className={classes.join(' ')}>Selam ŞAK BURDAYIM ,NEDEN ORDASIN</p>
+        <p className={assignedClasses.join(' ')}>Selam ŞAK BURDAYIM ,NEDEN ORDASIN</p>
         <button
-          className ="button"
+          className ={btnClass}
           onClick={this.togglePersonsHandler}
         >
           Göster kendini
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,37 +1,35 @@
 import React from 'react';
-// import './Person.css';
-import styled from "styled-components";
+import classes from './Person.css';
+// import styled from "styled-components";
 
 
 
+// const StyledDiv = styled.div `
 
+// background-color: green;
+// width: 60%;
+// margin:16px auto;
+// border: 1px solid #eee;
+// box-shadow: 0 2px 3px #ccc;
+// padding: 16px;
+// text-align: center;
 
-const StyledDiv = styled.div `
-
-background-color: red;
-width: 60%;
-margin:16px auto;
-border: 1px solid #eee;
-box-shadow: 0 2px 3px #ccc;
-padding: 16px;
-text-align: center;
-
-@media  (min-width : 500px)
-{
-width: 450px;
-}
-`;
+// @media  (min-width : 500px)
+// {
+// width: 450px;
+// }
+// `;
 
 
 const person = ( props ) => {
     
     return (
-        <StyledDiv>
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
 
-        </StyledDiv>
+        </div>
        
             
         
